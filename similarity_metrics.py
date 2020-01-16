4e18828d12221c1df08622c223d762cd85d7c3d4
WassimG <wassim.gabriel@gmail.com>
2020-01-16 15:21:17 +0100

    Updated python script

--- similarity_metrics.py
+++ similarity_metrics.py
@@ -5,55 +5,92 @@ from goatools.semantic import deepest_common_ancestor
 from goatools.semantic import semantic_similarity
 from goatools.semantic import TermCounts, get_info_content
 from goatools.semantic import lin_sim
+from goatools.anno.idtogos_reader import IdToGosReader
 
 
+def read_input(file_path):
 
-godag = GODag("go-basic.obo")
-fin_gaf = os.path.join(os.getcwd(), "tair.gaf")
-associations = dnld_assc(fin_gaf, godag)
+  """"
+  This function reads a csv with two columns of GO terms, coming from two datasets
+  Returns two lists of GO terms
+  """
 
-go_id3 = 'GO:0048364'
-go_id4 = 'GO:0032501'
+  GO_list1, GO_list2 = list(), list()
+  
+  with open(file_path, "r", encoding='utf-8-sig') as in_f: #other files might require other encoding?
+    for line in in_f:
+      if not line.startswith("GO"): # skip header
+        continue
+      GO1, GO2, _ = line.strip().split(",", maxsplit=2)
+      
+      # add GO1 to GO_list1
+      GO1_sub = list()
+      if ";" in GO1:
+        mGO = GO1.split(";")
+        for GO in mGO:
+          GO1_sub.append(GO)
+      else:
+        GO1_sub.append(GO1)
+      GO_list1.append(GO1_sub)
+      
 
-go_root = deepest_common_ancestor([go_id3, go_id4], godag)
+      # add GO2 to GO_list2
+      GO2_sub = list()
+      if ";" in GO2:
+        mGO = GO2.split(";")
+        for GO in mGO:
+          GO2_sub.append(GO)
+      else:
+        GO2_sub.append(GO2)
+      GO_list2.append(GO2_sub)
+    
+    return GO_list1, GO_list2
+	
+def compare_tools():
+	godag = GODag("go-basic.obo")
+	associations = IdToGosReader("data/associations-uniprot-sp-20200116.tab", godag=godag).get_id2gos('all')
 
-sim = semantic_similarity(go_id3, go_id4, godag)
-print('The semantic similarity between terms {} and {} is {}.'.format(go_id3, go_id4, sim))
+	# First get the counts of each GO term.
+	termcounts = TermCounts(godag, associations)
+	GO_list1, GO_list2 = read_input('data/example_input-compare_goa.csv')
+	for i in range(0,len(GO_list1)):
+	  BMA_test = BMA(GO_list1[i],GO_list2[i],termcounts,godag)
+	  print(BMA_test)
 
-# First get the counts of each GO term.
-termcounts = TermCounts(godag, associations)
-
-# Calculate the information content
-go_id = "GO:0008152"
-infocontent = get_info_content(go_id, termcounts)
-print('Information content ({}) = {}'.format(go_id, infocontent))
-
-
-sim_l = lin_sim(go_id3, go_id4, godag, termcounts)
-print('Lin similarity score ({}, {}) = {}'.format(go_id3, go_id4, sim_l))
-
-freq = termcounts.get_term_freq(go_id)
-
-def BMA(set1,set2,similarity_method):
+def BMA(GO_list1,GO_list2,termcounts,godag,similarity_method=None):
     summationSet12 = 0.0
     summationSet21 = 0.0
-    for id1 in set1:
+    for id1 in GO_list1:
         similarity_values = []
-        for id2 in set2:
+        for id2 in GO_list2:
             similarity_values.append(Rel_Metric(id1,id2,godag,termcounts))
         summationSet12 += max(similarity_values)
-    for id2 in set2:
+    for id2 in GO_list2:
         similarity_values = []
-        for id1 in set1:
+        for id1 in GO_list1:
             similarity_values.append(Rel_Metric(id2,id1,godag,termcounts))
         summationSet21 += max(similarity_values)
-    return (summationSet12+summationSet21)/(len(set1)+len(set2))
+    return (summationSet12+summationSet21)/(len(GO_list1)+len(GO_list2))
 
 def Rel_Metric(id1,id2,godag,termcounts):
-    mica_goid = deepest_common_ancestor([go_id1, go_id2], godag)
-    freq = termcounts.get_term_freq(mica_goid) 
-    infocontentMica = get_info_content(mica_goid, termcounts)
-    infocontent1 = get_info_content(go_id1, termcounts)
-    infocontent2 = get_info_content(go_id2, termcounts)
+    if id1 =='' or id2 =='':
+      return -1
+    goterm1 = godag[id1]
+    goterm2 = godag[id2]
+    if goterm1.namespace == goterm2.namespace:
+      mica_goid = deepest_common_ancestor([id1, id2], godag)
+      freq = termcounts.get_term_freq(mica_goid) 
+      infocontent = get_info_content(mica_goid, termcounts)
+      infocontent1 = get_info_content(id1, termcounts)
+      infocontent2 = get_info_content(id2, termcounts)
+      return (2*infocontent*(1-freq))/(infocontent1+infocontent2)
+    else:
+      return -1
 
-    return (2*infocontent*(1-freq))/(infocontent1+infocontent2)
+
+if __name__ == '__main__':
+    compare_tools()
+
+
+
+
