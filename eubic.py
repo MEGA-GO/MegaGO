59fb9a71526acb0aedf5c3a4485200b9140f1837
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2020-01-27 11:57:08 +0100

    unify main processing scripts in eubic.py

--- eubic.py
+++ eubic.py
@@ -5,42 +5,12 @@ from goatools.semantic import TermCounts, get_info_content
 import time
 import multiprocessing
 
-godag = GODag("go-basic2.obo")
+godag = GODag("go-basic.obo")
 
-def BMA(GO_list1, GO_list2, termcounts, similarity_method=None):
-    summationSet12 = 0.0
-    summationSet21 = 0.0
-    for id1 in GO_list1:
-        similarity_values = []
-        for id2 in GO_list2:
-            similarity_values.append(Rel_Metric(id1, id2, godag, termcounts))
-        summationSet12 += max(similarity_values)
-    for id2 in GO_list2:
-        similarity_values = []
-        for id1 in GO_list1:
-            similarity_values.append(Rel_Metric(id2, id1, godag, termcounts))
-        summationSet21 += max(similarity_values)
-    return (summationSet12 + summationSet21) / (len(GO_list1) + len(GO_list2))
-
-
-def Rel_Metric(id1, id2, godag, termcounts):
-    if id1 == '' or id2 == '':
-        return -1
-    goterm1 = godag[id1]
-    goterm2 = godag[id2]
-    if goterm1.namespace == goterm2.namespace:
-        mica_goid = deepest_common_ancestor([id1, id2], godag)
-        freq = termcounts.get_term_freq(mica_goid)
-        infocontent = get_info_content(mica_goid, termcounts)
-        infocontent1 = get_info_content(id1, termcounts)
-        infocontent2 = get_info_content(id2, termcounts)
-        return (2 * infocontent * (1 - freq)) / (infocontent1 + infocontent2)
-    else:
-        return -1
 
 
 def read_input(file_path):
-    """"
+    """
     This function reads a csv with two columns of GO terms, coming from two datasets
     Returns two lists of GO terms
     """
@@ -76,28 +46,64 @@ def read_input(file_path):
         return GO_list1, GO_list2
 
 
-def process(go1, go2, termcounts):
-    BMA_test = BMA(go1, go2, termcounts)
-    print(BMA_test)
+def BMA(GO_list1, GO_list2, termcounts, similarity_method=None):
+    summationSet12 = 0.0
+    summationSet21 = 0.0
+    for id1 in GO_list1:
+        similarity_values = []
+        for id2 in GO_list2:
+            similarity_values.append(Rel_Metric(id1, id2, godag, termcounts))
+        summationSet12 += max(similarity_values)
+    for id2 in GO_list2:
+        similarity_values = []
+        for id1 in GO_list1:
+            similarity_values.append(Rel_Metric(id2, id1, godag, termcounts))
+        summationSet21 += max(similarity_values)
+    return (summationSet12 + summationSet21) / (len(GO_list1) + len(GO_list2))
 
 
-GO_list1, GO_list2 = read_input('GO terms.csv')
-associations = IdToGosReader("uniprot-sp.tab", godag=godag).get_id2gos('all')
+def Rel_Metric(id1, id2, godag, termcounts):
+    if id1 == '' or id2 == '':
+        return -1
+    goterm1 = godag[id1]
+    goterm2 = godag[id2]
+    if goterm1.namespace == goterm2.namespace:
+        mica_goid = deepest_common_ancestor([id1, id2], godag)
+        freq = termcounts.get_term_freq(mica_goid)
+        infocontent = get_info_content(mica_goid, termcounts)
+        infocontent1 = get_info_content(id1, termcounts)
+        infocontent2 = get_info_content(id2, termcounts)
+        return (2 * infocontent * (1 - freq)) / (infocontent1 + infocontent2)
+    else:
+        return -1
 
-termcounts = TermCounts(godag, associations)
 
-jobs = []
+def main():
+    def process(go1, go2, termcounts):
+        BMA_test = BMA(go1, go2, termcounts)
+        print(BMA_test)
 
-start = time.time()
+    GO_list1, GO_list2 = read_input('data/example_input-compare_goa.csv')
+    associations = IdToGosReader("data/associations-uniprot-sp-20200116.tab", godag=godag).get_id2gos('all')
 
-for i in range(0, len(GO_list1)):
-    print(f"Computing {i}")
-    p = multiprocessing.Process(target=process, args=(GO_list1[i], GO_list2[i], termcounts))
-    jobs.append(p)
-    p.start()
+    termcounts = TermCounts(godag, associations)
 
-for job in jobs:
-    job.join()
+    jobs = []
 
-end = time.time()
-print(str(end - start) + " s")
+    start = time.time()
+
+    for i in range(0, len(GO_list1)):
+        print(f"Computing {i}")
+        p = multiprocessing.Process(target=process, args=(GO_list1[i], GO_list2[i], termcounts))
+        jobs.append(p)
+        p.start()
+
+    for job in jobs:
+        job.join()
+
+    end = time.time()
+    print(str(end - start) + " s")
+
+
+if __name__ == "__main__":
+    main()
