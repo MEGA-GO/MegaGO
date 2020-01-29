f2ede26ddfa5b98d4c9e792678583ca5178522d3
WassimG <wassim.gabriel@gmail.com>
2020-01-29 17:41:03 +0100

    Solved similarity bigger than 1 issue
    Getting the ancestor with the highest info content if the info content of a term is zero

--- megago/megago.py
+++ megago/megago.py
@@ -1,6 +1,7 @@
 from goatools.obo_parser import GODag
 from goatools.anno.idtogos_reader import IdToGosReader
 from goatools.semantic import deepest_common_ancestor, get_info_content, TermCounts
+from goatools.gosubdag.gosubdag import GoSubDag
 import time
 import multiprocessing
 
@@ -81,6 +82,15 @@ def BMA(GO_list1, GO_list2, termcounts, similarity_method=None):
         summationSet21 += max(similarity_values)
     return (summationSet12 + summationSet21) / (len(GO_list1) + len(GO_list2))
 
+def get_highest_ic_anc(id,termcounts):
+    gosubdag_r0 = GoSubDag([id], GODAG, prt=None)
+    P = gosubdag_r0.rcntobj.go2parents[id]
+    max_ic = 0
+    for i in P:
+        ic = get_info_content(i, termcounts)
+        if(max_ic < ic):
+            max_ic = ic
+    return max_ic
 
 def Rel_Metric(id1, id2, godag, termcounts):
     if id1 == '' or id2 == '':
@@ -90,10 +100,14 @@ def Rel_Metric(id1, id2, godag, termcounts):
     if goterm1.namespace == goterm2.namespace:
         mica_goid = deepest_common_ancestor([id1, id2], godag)
         freq = termcounts.get_term_freq(mica_goid)
-        infocontent = get_info_content(mica_goid, termcounts)
-        infocontent1 = get_info_content(id1, termcounts)
-        infocontent2 = get_info_content(id2, termcounts)
-        return (2 * infocontent * (1 - freq)) / (infocontent1 + infocontent2)
+        info_content = get_info_content(mica_goid, termcounts)
+        info_content1 = get_info_content(id1, termcounts)
+        info_content2 = get_info_content(id2, termcounts)
+        if(info_content1 == 0):
+             info_content1 = get_highest_ic_anc(id1,termcounts)
+        if(info_content2 == 0):
+             info_content2 = get_highest_ic_anc(id2,termcounts)
+        return (2 * info_content * (1 - freq)) / (info_content1 + info_content2)
     else:
         return -1
 
