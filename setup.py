dbcee218da9956099c3e5e021e2a3d14850bf49e
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2020-02-12 15:03:30 +0100

    bump to v0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ and is an ideal starting place for new projects.'''
 
 setup(
     name='megago',
-    version='0.1.0',
+    version='0.2.0',
     author='Henning Schiebenhoefer',
     author_email='henning.schiebenhoefer@posteo.de',
     packages=['megago'],
