a1847e54b882bd1e5ee3cf245bee25240cb0a20a
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-06-16 15:02:30 +0200

    Add placeholders for about and help

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,34 @@
 <template>
     <v-app>
         <v-app-bar app color="primary" dark>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="navigationDrawer = !navigationDrawer"></v-app-bar-nav-icon>
             <v-toolbar-title>Mega GO</v-toolbar-title>
         </v-app-bar>
+        <v-navigation-drawer app v-model="navigationDrawer">
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Mega GO
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Compute similarity
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav class="py-0 mt-4">
+                <v-list-item v-for="item of links" :key="item.link" link :to="item.link">
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
         <v-main class="mx-xs-2 mx-sm-4 mx-md-16">
             <v-container fluid>
                 <router-view></router-view>
@@ -18,5 +43,24 @@ import Component from "vue-class-component";
 
 @Component
 export default class App extends Vue {
+    private navigationDrawer = false;
+
+    private links = [
+        {
+            icon: "mdi-home",
+            title: "Home",
+            link: "/"
+        },
+        {
+            icon: "mdi-domain",
+            title: "About",
+            link: "/about"
+        },
+        {
+            icon: "mdi-help-circle-outline",
+            title: "Help",
+            link: "/help"
+        }
+    ]
 }
 </script>
