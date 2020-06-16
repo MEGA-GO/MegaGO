79196fae0409bd2582f38e3d8b266f26cc79265b
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-06-16 14:45:43 +0200

    Basic project for our web app

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>Mega GO</v-toolbar-title>
+        </v-app-bar>
+        <v-main class="mx-xs-2 mx-sm-4 mx-md-16">
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
 
-#nav {
-  padding: 30px;
+@Component
+export default class App extends Vue {
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="text-h3">Welcome</div>
+        <div class="text-body-1">
+            Mega GO is a project that started in January, 2020 in Nyborg, Denmark. Our project aims at computing the
+            similarity between two sets of GO-terms.
+        </div>
+<!--        <img src="./../assets/logo.png" alt="Mega GO logo" class="mt-12"/>-->
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+@Component
+export default class Home extends Vue {
+
 }
 </script>
+
+<style scoped>
+
+</style>
