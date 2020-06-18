37003f32e871509eae828c199e7b33c017bf58a8
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-06-18 11:11:25 +0200

    Display extra information about GO terms

--- web/src/components/GoListComponent.vue
+++ web/src/components/GoListComponent.vue
@@ -1,21 +1,35 @@
 <template>
-    <v-virtual-scroll height="300" :items="items" :item-height="50">
+    <div style="height: 300px;" v-if="loading">
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+    <v-virtual-scroll v-else height="300" :items="items" :item-height="50" :bench="50">
         <template v-slot="{ item }">
             <v-list-item>
-                <v-list-item-avatar>
-                    <v-avatar color="primary" size="56" class="white--text">
-                        GO
-                    </v-avatar>
-                </v-list-item-avatar>
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-list-item-avatar>
+                            <v-avatar v-on="on" v-bind="attrs" :color="namespaceColor[item.namespace]" size="56" class="white--text">
+                                {{ namespaceAvatar[item.namespace] }}
+                            </v-avatar>
+                        </v-list-item-avatar>
+                    </template>
+                    <span>{{ humanReadableNamespace(item.namespace) }}</span>
+                </v-tooltip>
 
                 <v-list-item-content>
-                    <v-list-item-title>{{ item.code }}</v-list-item-title>
+                    <v-list-item-title>{{ item.code }} - {{ item.name }}</v-list-item-title>
                 </v-list-item-content>
 
                 <v-list-item-action>
-                    <v-btn depressed small :href="`http://amigo.geneontology.org/amigo/term/${item.code}`" target="_blank">
+                    <v-btn
+                        depressed
+                        small
+                        :href="`http://amigo.geneontology.org/amigo/term/${item.code}`"
+                        target="_blank">
                         View term
-                        <v-icon color="orange darken-4" right>
+                        <v-icon
+                            color="orange darken-4"
+                            right>
                             mdi-open-in-new
                         </v-icon>
                     </v-btn>
@@ -29,6 +43,7 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop, Watch } from "vue-property-decorator";
+import APICommunicator from "@/logic/network/APICommunicator";
 
 @Component
 export default class GoListComponent extends Vue {
@@ -36,19 +51,46 @@ export default class GoListComponent extends Vue {
     private terms!: string[];
 
     private items: { code: string }[] = [];
+    private loading = false;
+
+    private namespaceAvatar = {
+        // eslint-disable-next-line quote-props
+        "biological_process": "BP",
+        // eslint-disable-next-line quote-props
+        "cellular_component": "CC",
+        // eslint-disable-next-line quote-props
+        "molecular_function": "MF"
+    }
+
+    private namespaceColor = {
+        // eslint-disable-next-line quote-props
+        "biological_process": "indigo",
+        // eslint-disable-next-line quote-props
+        "cellular_component": "red",
+        // eslint-disable-next-line quote-props
+        "molecular_function": "green"
+    }
 
     private mounted() {
         this.onTermsChanged();
     }
 
     @Watch("terms")
-    private onTermsChanged() {
+    private async onTermsChanged() {
+        this.loading = true;
         this.items.splice(0, this.items.length);
-        this.items.push(...this.terms.map((id) => {
-            return {
-                code: id
-            };
-        }));
+        this.items.push(...(await APICommunicator.getGoTerms(this.terms)));
+        this.loading = false;
+    }
+
+    private humanReadableNamespace(ns: string): string {
+        return ns.split("_").map(x => {
+            if (x.length > 1) {
+                return x[0].toUpperCase() + x.slice(1).toLowerCase();
+            } else {
+                return x.toUpperCase();
+            }
+        }).join(" ");
     }
 }
 </script>
--- web/src/logic/network/APICommunicator.ts
+++ web/src/logic/network/APICommunicator.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import SimilarityResponse from "@/logic/network/SimilarityResponse";
+import GoTerm from "@/logic/entities/GoTerm";
 
 export default class APICommunicator {
     public static readonly BASE_URL: string = "http://localhost:5000";
@@ -10,5 +11,13 @@ export default class APICommunicator {
             sample2: go2
         });
         return result.data;
+    }
+
+    public static async getGoTerms(terms: string[]): Promise<GoTerm[]> {
+        const result = await axios.post(`${APICommunicator.BASE_URL}/goterms`, {
+            goterms: terms
+        });
+
+        return result.data.goterms;
     }
 }
