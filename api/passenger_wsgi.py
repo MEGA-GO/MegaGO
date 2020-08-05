3b22adb59b6405b24af666e05f1258a85bf7f534
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-08-05 15:59:49 +0200

    More changes to the container

--- api/passenger_wsgi.py
+++ api/passenger_wsgi.py
@@ -1,2 +1,12 @@
+import imp
+import os
+
+from pathlib import Path
+
+app = imp.load_source(
+    'app',
+    os.path.join(Path(__file__).parent, 'app.py')
+)
+
 # Required for passenger to work and connect with this through the WSGI interface.
 from app import app as application
