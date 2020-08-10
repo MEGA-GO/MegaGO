7caa9a68fac3b812d26e6050fd5b78324e18e44e
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-08-10 14:43:14 +0200

    Add content to the 'About us' page

---
+++ web/src/components/TeamCard.vue
@@ -0,0 +1,100 @@
+<template>
+    <v-card class="profile-card">
+        <div class="profile-card_cover">
+            <img src="./../assets/abstract_background.png" alt="profile background">
+        </div>
+        <div class="profile-card_padding">
+            <div class="profile-card_image">
+                <img :src="profilePicture" alt="profile picture">
+            </div>
+            <div class="profile-card_details">
+                <h3>{{ name }}</h3>
+                <p class="text-muted">{{ affiliation }}</p>
+            </div>
+        </div>
+    </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Component
+export default class TeamCard extends Vue {
+    @Prop({ required: true })
+    private name: string;
+
+    @Prop({ required: true })
+    private affiliation: string;
+
+    @Prop({ required: false, default: require("./../assets/profile_picture.jpg") })
+    private profilePicture: string;
+}
+</script>
+
+<style scoped>
+    .profile-card {
+        background: #fff;
+        -webkit-box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1);
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    .profile-card .profile-card_cover {
+        height: 130px;
+        overflow: hidden;
+    }
+
+    .profile-card .profile-card_cover img {
+        width: 100%;
+        height: 100%;
+        -o-object-fit: cover;
+        object-fit: cover;
+    }
+
+    .profile-card .profile-card_padding {
+        padding: 25px;
+        position: relative;
+        margin-top: -85px;
+    }
+
+    .profile-card .profile-card_image {
+        width: 125px;
+        height: 125px;
+        border-radius: 125px;
+        overflow: hidden;
+        border: 4px solid #fff;
+        margin: 0 auto 15px auto;
+        -webkit-box-shadow: 0 5px 30px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 5px 30px rgba(0, 0, 0, 0.1);
+    }
+
+    .profile-card .profile-card_image img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .profile-card .profile-card_details h3 {
+        font-size: 21px;
+    }
+
+    .profile-card .profile-card_details p {
+        font-size: 16px;
+        margin-bottom: 15px;
+    }
+
+    .profile-card .profile-card_social {
+        text-align: center;
+    }
+
+    .profile-card .profile-card_social a {
+        display: inline-block;
+    }
+
+    .profile-card .profile-card_social img {
+        width: 33px;
+        margin: 5px 10px;
+    }
+</style>
--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -3,11 +3,52 @@
         <div>
             <div class="text-h3">About us</div>
             <p class="text-body-1">
-                Mega GO is a project that aims at comparing two sets of GO terms and started initially at the EuBIC conference
-                in Nyborg, Denmark (2020).
+                Mega GO is a project that aims at comparing two sets of GO terms and started initially at the EuBIC
+                conference in Nyborg, Denmark (2020). The Mega GO project aims to provide users with a measure that
+                indicates how similar two sets of GO terms are to each other.
             </p>
             <div class="text-h4">Our team</div>
-
+            <v-container fluid>
+                <v-row>
+                    <v-col :cols="4">
+                        <team-card name="Henning Schiebenhoefer" affiliation="Robert Koch Institut, Germany"></team-card>
+                    </v-col>
+                    <v-col :cols="4">
+                        <team-card
+                            name="Tim Van Den Bossche"
+                            affiliation="Ghent University, Belgium"
+                            :profile-picture="require('./../assets/tim_profile.jpg')">
+                        </team-card>
+                    </v-col>
+                    <v-col :cols="4">
+                        <team-card
+                            name="Pieter Verschaffelt"
+                            affiliation="Ghent University, Belgium"
+                            :profile-picture="require('./../assets/pieter_profile.jpg')">
+                        </team-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col :cols="4">
+                        <team-card
+                            name="Bart Mesuere"
+                            affiliation="Ghent University, Belgium"
+                            :profile-picture="require('./../assets/bart_profile.jpeg')">
+                        </team-card>
+                    </v-col>
+                    <v-col :cols="4">
+                        <team-card name="Wassim Gabriel" affiliation="Technische Universität München, Germany"></team-card>
+                    </v-col>
+                    <v-col :cols="4">
+                        <team-card name="Michał Burdukiewicz" affiliation="Warsaw University of Technology, Poland"></team-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col :cols="4">
+                        <team-card name="Alessio Soggiu" affiliation="University of Milan, Italy"></team-card>
+                    </v-col>
+                </v-row>
+            </v-container>
         </div>
     </v-container>
 
@@ -16,8 +57,10 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-
-@Component
+import TeamCard from "@/components/TeamCard.vue";
+@Component({
+    components: { TeamCard }
+})
 export default class About extends Vue {
 
 }
