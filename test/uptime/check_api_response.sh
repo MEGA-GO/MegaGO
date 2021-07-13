449767cc540e67f9aeb54ccb2be5e499de5fc087
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2021-07-13 09:52:34 +0200

    Add automatic check for uptime

---
+++ test/uptime/check_api_response.sh
@@ -0,0 +1,17 @@
+# This script checks if the API-server returns results as expected, and can thus be used by GitHub actions to
+# periodically verify that our server is still up and running
+
+set -e
+set -o pipefail
+
+RESULTS=$(curl --silent megago.ugent.be 2> /dev/null | grep "MegaGO")
+
+# Check that the command returns a valid URL
+if ! [[ -z $RESULTS ]]
+then
+  echo "MegaGO did not respond properly." >&2
+  exit 1
+fi
+
+# All is well!
+exit 0
